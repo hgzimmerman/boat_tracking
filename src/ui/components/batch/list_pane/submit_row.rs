f6795d4ad714ac9703ce73022e49527e41f43117
Henry Zimmerman <zimhen7@gmail.com>
2024-04-07 14:11:44 -0400

    misc button styling

--- src/ui/components/batch/list_pane/submit_row.rs
+++ src/ui/components/batch/list_pane/submit_row.rs
@@ -55,68 +55,71 @@ fn SessionTypeDropup(
 ) -> Element {
     let mut show_session_type_dropdown = use_signal(|| false);
     rsx! {
-        button {
-            id: "session-dropdown-btn",
-            class: "btn btn-blue min-w-44 rounded-s",
-            onclick: move |e| {
-                e.stop_propagation();
-                if !mode.is_view() {
-                    let inverted = !*show_session_type_dropdown.read();
-                    show_session_type_dropdown.set(inverted);
-                }
-            },
-            onmouseover: move |e| {
-                e.stop_propagation();
-                if !mode.is_view() {
-                    show_session_type_dropdown.set(true);
-                }
-            },
-            onmouseout: move |e| {
-                e.stop_propagation();
-                if !mode.is_view() {
-                    show_session_type_dropdown.set(false);
-                }
-            },
-            {session_type.read().to_string()}
-            // the dropdown
-            div {
-                id: "session-dropdown-positioner",
-                class: "relative h-0 w-0 top-2",
+        div {
+            class: "my-0",
+            button {
+                id: "session-dropdown-btn",
+                class: "btn min-w-44 rounded-lg bg-gray-700 text-white",
+                onclick: move |e| {
+                    e.stop_propagation();
+                    if !mode.is_view() {
+                        let inverted = !*show_session_type_dropdown.read();
+                        show_session_type_dropdown.set(inverted);
+                    }
+                },
+                onmouseover: move |e| {
+                    e.stop_propagation();
+                    if !mode.is_view() {
+                        show_session_type_dropdown.set(true);
+                    }
+                },
+                onmouseout: move |e| {
+                    e.stop_propagation();
+                    if !mode.is_view() {
+                        show_session_type_dropdown.set(false);
+                    }
+                },
+                {session_type.read().to_string()}
+                // the dropdown
                 div {
-                    id: "session-dropdown",
-                    class: if *show_session_type_dropdown.read() {
-                        "absolute z-10 py-2 w-40 bottom-8 left-4 origin-bottom-right rounded-md bg-white shadow-lg divide-y m-2 text-slate-600 font-normal"
-                    } else {
-                        "hidden"
-                    },
-                    ul {
-                        {
-                            [
-                                UseScenario::YouthGgrc,
-                                UseScenario::YouthSomerville,
-                                UseScenario::Adult,
-                                UseScenario::LearnToRow,
-                                UseScenario::ScullingSaturday,
-                                UseScenario::PrivateSession,
-                                UseScenario::Regatta,
-                                UseScenario::Other
-                            ]
-                            .into_iter()
-                            .map(|use_scenario| {
-                                let active = session_type() == use_scenario;
-                                rsx!{
-                                    li {
-                                        class: "hover:bg-slate-300",
-                                        class: if active{"bg-slate-200"},
-                                        onclick: move |e| {
-                                            e.stop_propagation();
-                                            session_type.set(use_scenario);
-                                            show_session_type_dropdown.set(false);
-                                        },
-                                        {use_scenario.to_string()}
+                    id: "session-dropdown-positioner",
+                    class: "relative h-0 w-0 top-2",
+                    div {
+                        id: "session-dropdown",
+                        class: if *show_session_type_dropdown.read() {
+                            "absolute z-10 py-2 w-40 bottom-8 left-4 origin-bottom-right rounded-md bg-white shadow-lg divide-y m-2 text-slate-600 font-normal"
+                        } else {
+                            "hidden"
+                        },
+                        ul {
+                            {
+                                [
+                                    UseScenario::YouthGgrc,
+                                    UseScenario::YouthSomerville,
+                                    UseScenario::Adult,
+                                    UseScenario::LearnToRow,
+                                    UseScenario::ScullingSaturday,
+                                    UseScenario::PrivateSession,
+                                    UseScenario::Regatta,
+                                    UseScenario::Other
+                                ]
+                                .into_iter()
+                                .map(|use_scenario| {
+                                    let active = session_type() == use_scenario;
+                                    rsx!{
+                                        li {
+                                            class: "hover:bg-slate-300",
+                                            class: if active{"bg-slate-200"},
+                                            onclick: move |e| {
+                                                e.stop_propagation();
+                                                session_type.set(use_scenario);
+                                                show_session_type_dropdown.set(false);
+                                            },
+                                            {use_scenario.to_string()}
+                                        }
                                     }
-                                }
-                            })
+                                })
+                            }
                         }
                     }
                 }
@@ -133,7 +136,7 @@ fn TimeSelector(
 ) -> Element {
     rsx! {
         div {
-            class: "my-1",
+            class: "my-0",
             input {
                 r#type: "datetime-local",
                 id: "manufactured-at",
